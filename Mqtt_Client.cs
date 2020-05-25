5e7570c489d9b3ab5d0c0678febe062e32e5d4e7
akileswaran <akileswaran.karthikeyan@gmail.com>
2020-05-25 13:52:12 +0200

    client code for OPC and MQTT com

---
+++ Mqtt_Client.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using uPLibrary.Networking.M2Mqtt;
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+public class mqtt : MonoBehaviour
+{
+    //http://tdoc.info/blog/2014/11/10/mqtt_csharp.html
+    private MqttClient4Unity client;
+
+    public string brokerHostname = null;
+    public int brokerPort = 1883;
+    public string userName = null;
+    public string password = null;
+    public string topic = null;
+
+    private Queue msgq = new Queue();
+
+   string lastMessage = null;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (brokerHostname != null && userName != null && password != null)
+        {
+            Connect();
+            client.Subscribe(topic);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (client.Count() > 0)
+        {
+            //for(int i =0;i<100;i++)
+            //{
+                string s = client.Receive();
+                msgq.Enqueue(s);
+                // Debug.Log("received :" + s);
+                Console.WriteLine(s);
+            if (s.Contains("rack1:"))
+            {
+                TextMesh textObject = GameObject.Find("rack1:").GetComponent<TextMesh>();
+                textObject.text = s;
+            }
+            if (s.Contains("rack2:"))
+            {
+                TextMesh textObject = GameObject.Find("rack2:").GetComponent<TextMesh>();
+                textObject.text = s;
+            }
+            if (s.Contains("rack3:"))
+            {
+                TextMesh textObject = GameObject.Find("rack3:").GetComponent<TextMesh>();
+                textObject.text = s;
+            }
+
+            // }
+
+
+        }
+
+    }
+
+
+
+    public void Connect()
+    {
+        client = new MqttClient4Unity(brokerHostname, brokerPort, false, null);
+        string clientId = Guid.NewGuid().ToString();
+        client.Connect(clientId, userName, password);
+    }
+
+    public void Publish(string _topic, string msg)
+    {
+        client.Publish(
+            _topic, Encoding.UTF8.GetBytes(msg),
+            MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE, false);
+    }
+}
+
+
---
+++ OpcUaCom.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Opc.Ua;
+using Opc.Ua.Client;
+using Opc.Ua.Configuration;
+using System;
+using System.IO;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class OpcUaCom : MonoBehaviour
+{
+    
+    public string _opcUaEndpointUrl;
+
+    static private List<string> opcUaEndpointList;
+    
+    private float countdown = 0.5f;
+  
+
+    private OpcUaClient _opcUaClient;
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        opcUaEndpointList = new List<string>();
+        opcUaEndpointList.Add("ns=4;s=MAIN.rack1");
+        opcUaEndpointList.Add("ns=4;s=MAIN.rack2");     
+        opcUaEndpointList.Add("ns=4;s=MAIN.rack3");
+        opcUaEndpointList.Add("ns=4;s=MAIN.rack4");
+
+
+
+        _opcUaClient = new OpcUaClient();
+         
+        _opcUaClient.SubscripeToOpcUaServer(_opcUaEndpointUrl, opcUaEndpointList);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+           
+        if (countdown <= 0)
+        {
+            _opcUaClient.WriteOpcUaFloatNode("ns=4;s=MAIN.rack2", 50);
+            _opcUaClient.WriteOpcUaNode("ns=4;s=MAIN.rack1", true);
+
+
+            var Num = _opcUaClient.session.ReadValue("ns=4;s=MAIN.rack3");
+            var Numb = _opcUaClient.session.ReadValue("ns=4;s=MAIN.rack4");
+            TextMesh textObject = GameObject.Find("rack1").GetComponent<TextMesh>();
+            textObject.text = "test";
+            TextMesh textObject1 = GameObject.Find("rack2").GetComponent<TextMesh>();
+            textObject1.text ="iyg";
+
+            TextMesh textObject2 = GameObject.Find("rack3").GetComponent<TextMesh>();
+            textObject2.text =Convert.ToString(Num);
+            TextMesh textObject3 = GameObject.Find("rack4").GetComponent<TextMesh>();
+            textObject3.text = Convert.ToString(Numb);
+
+            countdown = 0.5f;
+        }
+        countdown -= Time.deltaTime;
+
+    }
+}
+
